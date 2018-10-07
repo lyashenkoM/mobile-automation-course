c67c33f1a778dd31c7426460714fb8335ef6f1ae
lyashenkoM <maria@lyashenko.by>
2018-10-07 21:48:37 +0300

    Task 3
    Task 3 result

--- firstTasks/src/MainClassTest.java
+++ firstTasks/src/MainClassTest.java
@@ -4,8 +4,9 @@ import org.junit.Assert;
 public class MainClassTest {
 
     @Test
-    public void testGetClassNumber(){
-        MainClass returnValue = new MainClass();
-        Assert.assertTrue("Return value < 45",returnValue.getClassNumber()>45);
+    public void testGetClassString(){
+        String returnValue = new MainClass().getClassString();
+        boolean result = returnValue.contains("Hello") || returnValue.contains("hello");
+        Assert.assertTrue("String does not contain 'Hello' or 'hello'", result);
     }
 }
