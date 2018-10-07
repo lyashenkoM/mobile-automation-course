6058db5a2e38e9e5f40d1a6ffec690330f1c7511
lyashenkoM <maria@lyashenko.by>
2018-10-07 19:39:49 +0300

    Test 2
    Changes with Test 2

--- firstTasks/src/MainClassTest.java
+++ firstTasks/src/MainClassTest.java
@@ -2,9 +2,10 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class MainClassTest {
+
     @Test
-    public void testGetLocalNumber(){
+    public void testGetClassNumber(){
         MainClass returnValue = new MainClass();
-        Assert.assertTrue("Return value !=14",returnValue.getLocalNumber()==14);
+        Assert.assertTrue("Return value < 45",returnValue.getClassNumber()>45);
     }
 }
